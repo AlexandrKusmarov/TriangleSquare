ffc7e83254cb608f1c7ff2223f349556a692b250
Alexandr Kusmarov <Kusmarov_a@mail.ru>
2019-10-09 12:53:20 +0300

    Creation of the project

---
+++ src/main/java/com/domain/TriangleSquare/TriangleSearcher.java
@@ -0,0 +1,10 @@
+package com.domain.TriangleSquare;
+
+
+public class TriangleSearcher 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
